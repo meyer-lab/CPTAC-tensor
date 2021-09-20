520b0cb55551a9ae5c85d4aa5c3b850d99af186f
cztan <cyztan@gmail.com>
2021-09-20 15:37:51 -0700

    Add tensor runs to test

--- cpactensor/tensor.py
+++ cpactensor/tensor.py
@@ -5,7 +5,7 @@ from .data import gen_tensor_matrix
 def decomp_3d_2d():
     (tensor, geneSet, patients, _), (matrix, clust_data, _) = gen_tensor_matrix()
     tensor = np.swapaxes(tensor, 0, 1)
-    for rr in np.arange(1,5):
+    for rr in np.arange(1,2):
         tFac = perform_CMTF(tensor, matrix, r=rr)
         print(rr, tFac.R2X)
 
