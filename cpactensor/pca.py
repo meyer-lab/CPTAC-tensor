322806acf22932dc3d16c66a4fce89b1ab1dec56
eniohodzic <ehodzic@g.ucla.edu>
2021-11-11 19:58:51 -0800

    Script, plots R2X and PCA against data size

--- cpactensor/pca.py
+++ cpactensor/pca.py
@@ -6,45 +6,53 @@ from tensorpac import perform_CP, calcR2X
 from statsmodels.multivariate.pca import PCA
 
 # Comparing tensor decomp vs PCA 
-def pca_vs_tensor():
+#def pca_vs_tensor():
     
-    ### Import data, into Tensor
-    (tensor, variables, subjects, TvN) = gen_concat_tensor()
+### Import data, into Tensor
+(tensor, variables, subjects, TvN) = gen_concat_tensor()
 
-    # Convert tensor to boolean array, 1 = non_missing
-    logicTensor = np.logical_not(np.isnan(tensor))
+# Convert tensor to boolean array, 1 = non_missing
+logicTensor = np.logical_not(np.isnan(tensor))
 
-    # Remove rows with all NaNs
-    idxs = []
-    for i in range(logicTensor.shape[0]):
-        for j in range(logicTensor.shape[1]):
-            if np.sum(logicTensor[i][j][:]) < 25:
-                idxs.append(j)
-    tensorNoNan = np.delete(tensor, idxs, axis=1)
+# Remove rows with all NaNs
+idxs = []
+for i in range(logicTensor.shape[0]):
+    for j in range(logicTensor.shape[1]):
+        if np.sum(logicTensor[i][j][:]) < 25:
+            idxs.append(j)
+tensorNoNan = np.delete(tensor, idxs, axis=1)
 
-    # Unfold along dim=0, TvN
-    tensorShape = tensorNoNan.shape
-    flatTensor = np.reshape(tensorNoNan, (tensorShape[0]*tensorShape[1] , tensorShape[2]))
+# Unfold along dim=0, TvN
+tensorShape = tensorNoNan.shape
+flatTensor = np.reshape(tensorNoNan, (tensorShape[0]*tensorShape[1] , tensorShape[2]))
 
-    # Choose random idxs along vars axis
-    idxss = np.random.randint(flatTensor.shape[1], size=500)
+# Choose random idxs along vars axis
+num = 300
+idxss = np.random.randint(flatTensor.shape[1], size=num)
 
-    randFlatTensor = flatTensor[:,idxss]
-    randTensor = tensorNoNan[:,:,idxss]
-    randVars = np.take(variables,idxss)
+randFlatTensor = flatTensor[:,idxss]
+randTensor = tensorNoNan[:,:,idxss]
+randVars = np.take(variables,idxss)
 
-    comps = np.arange(1,5)
-    CPR2X = np.zeros(comps.shape)
-    PCAR2X = np.zeros(comps.shape)
+comps = np.arange(1,12)
+CPR2X = np.zeros(comps.shape)
+PCAR2X = np.zeros(comps.shape)
+CPSize = np.zeros(comps.shape)
+PCASize = np.zeros(comps.shape)
 
-    for c, i in enumerate(comps):
-        outt = PCA(randFlatTensor, ncomp=i, missing='fill-em', standardize=False, demean=False, normalize=False)
-        recon = outt.scores @ outt.loadings.T
-        PCAR2X[c] = calcR2X(recon, mIn=randFlatTensor)
-        tfac = perform_CP(randTensor, r=i)
-        CPR2X[c] = tfac.R2X
+for c, i in enumerate(comps):
+    outt = PCA(randFlatTensor, ncomp=i, missing='fill-em', standardize=False, demean=False, normalize=False)
+    recon = outt.scores @ outt.loadings.T
+    PCASize[c] = sum(randFlatTensor.shape) * i
+    PCAR2X[c] = calcR2X(recon, mIn=randFlatTensor)
+    tfac = perform_CP(randTensor, r=i)
+    CPSize[c] = sum(tfac.shape) * i 
+    CPR2X[c] = tfac.R2X
 
-    plt.scatter(comps, CPR2X, c='blue')
-    plt.scatter(comps, PCAR2X, c='red')
-    plt.show()
-    
+plt.scatter(CPSize, CPR2X, c='blue', label='Tensor')
+plt.scatter(PCASize, PCAR2X, c='red', label='PCA')
+plt.xlabel("Size of reduced data")
+plt.ylabel("R2X")
+plt.title(num, " variables chosen at random")
+plt.legend()
+plt.show()
